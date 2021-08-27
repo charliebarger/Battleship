351adb63107a51237581fc3f2bc23e973fd5b8ec
charliebarger <charliebarger04@gmail.com>
2021-08-27 16:42:17 -0600

    initial commit

--- src/componenets/GridWrapper.js
+++ src/componenets/GridWrapper.js
@@ -1,6 +1,10 @@
 import React from "react";
 const GridWrapper = () => {
-  return <div className="grid-wrapper"></div>;
+  return (
+    <div className="grid-wrapper">
+      <div></div>
+    </div>
+  );
 };
 
 export default GridWrapper;
